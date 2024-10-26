0d4dd1fb143b9136a199786b1096afcb5d8e6344
pip333 <93560502+pip333@users.noreply.github.com>
2024-10-26 14:43:32 +0800

    Update get-token.js

--- api/get-token.js
+++ api/get-token.js
@@ -1,10 +1,19 @@
 // /api/get-token.js
 
 export default function handler(req, res) {
-    // 从 Vercel 环境变量中获取 token
-    const token = process.env.OAI_REFRESH_TOKEN;
-    if (!token) {
-        return res.status(500).json({ error: 'Token not found' });
+    const { code } = req.query;
+
+    // 判断输入是否为 "zk"
+    if (code === 'zk') {
+        // 从环境变量中获取 token
+        const token = process.env.OAI_REFRESH_TOKEN;
+
+        if (token) {
+            res.status(200).json({ token });
+        } else {
+            res.status(500).json({ error: 'Token not found in environment variables' });
+        }
+    } else {
+        res.status(403).json({ error: 'Unauthorized access' });
     }
-    res.status(200).json({ token });
 }
